2b20bbb1d845f11c722b2ac789300fdb7966c9a4
Chinwendu <nwazojiechinwendu@gmail.com>
2025-04-12 23:57:29 +0100

    assignment 3

--- Assignment 3/index.js
+++ Assignment 3/index.js
@@ -1,1 +1,43 @@
-console.log('hello')
+const name = 'Chinwendu'
+const age = '20'
+const nationality = 'Nigerian'
+
+let myName = "Hello, my name is " + name + ", I am "+ age + " years old. I am a " +nationality
+
+console.log(myName)
+
+let myQuote = "God is love";
+
+myQuote.toUpperCase
+
+console.log(myQuote.toUpperCase)
+
+
+
+
+
+
+
+let favoiriteFood = [
+    'Rice', 'Beans', 'Ukwa', 'Achicha', 'Igba'
+]
+
+console.log(favoiriteFood[0])
+console.log(favoiriteFood[4])
+
+favoiriteFood.push('JILL')
+
+console.log(favoiriteFood)
+
+
+
+
+const jssOne = ['Henry', 'Daveid', 'linus', 'john', 'Tom', 'Ekpan', 'Geet', 'Maan', 'Dev', 'Sid']
+const jssTwo = ['Fatih', 'Monsu', 'Adi', 'Sasha', 'Chu', 'Emeka', 'Emma', 'Ik', 'Enu', 'Jill']
+const jssThree = ['Kunmi', 'Jelil', 'Jemila', 'Dicta', 'Ify', 'Amaka', 'Arinxe', 'Ben', 'Fort', 'Evan']
+
+
+
+
+
+
