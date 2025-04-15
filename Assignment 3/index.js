2be630a2f9bf3a4477c38deff4d99296d5dc4ce1
Chinwendu <nwazojiechinwendu@gmail.com>
2025-04-15 11:46:21 +0100

    new updates

--- Assignment 3/index.js
+++ Assignment 3/index.js
@@ -1,3 +1,5 @@
+// Create variables to store your name, age, and nationality. Combine them into a sentence that introduces you.
+
 const name = 'Chinwendu'
 const age = '20'
 const nationality = 'Nigerian'
@@ -7,7 +9,6 @@ let myName = "Hello, my name is " + name + ", I am "+ age + " years old. I am a 
 console.log(myName)
 // Make a variable with your favorite quote or lyric. Change the entire text to uppercase and lowercase in separate steps.
 
-
 let myQuote = "God is love";
 
 myQuote.toLowerCase
@@ -16,11 +17,13 @@ console.log(myQuote.toUpperCase())
 console.log(myQuote.toLowerCase())
 
 
+// Take a word and rearrange it in reverse order, then log the reversed word.
+
+console.log()
 
 
 
-
-// 6.Create an array of your 5 favorite foods. Display the first and last items in the array.
+// Create an array of your 5 favorite foods. Display the first and last items in the array.
 
 
 let favoiriteFood = [
@@ -29,6 +32,8 @@ let favoiriteFood = [
 
 console.log(favoiriteFood[0])
 console.log(favoiriteFood[4])
+
+// Add two more food items to the list: one at the beginning and one at the end. Display the new list.
 
 favoiriteFood.push('JILL')
 
@@ -40,8 +45,22 @@ let lambo = 70
 let Kit = 100
 let cloth = 87
 
-console.log(sum)
+const sum = lambo + Kit+ cloth
 
+console.log("I will need a total of N" +sum)
+
+// Find the average of 5 test scores stored in variables. Display the average with a message.
+
+const h =5
+const d =5
+const u =5
+const p =5
+const t =5
+
+
+const aveg = (h+d+u+p+t)/ 5
+
+console.log("The average is " +aveg)
 
 
 // Create three arrays:
