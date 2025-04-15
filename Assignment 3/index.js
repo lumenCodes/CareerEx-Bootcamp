a8e2f4d4c170d73cfd1f93787b32f8d1a240af38
Chinwendu <nwazojiechinwendu@gmail.com>
2025-04-15 11:07:04 +0100

    new updates

--- Assignment 3/index.js
+++ Assignment 3/index.js
@@ -5,17 +5,22 @@ const nationality = 'Nigerian'
 let myName = "Hello, my name is " + name + ", I am "+ age + " years old. I am a " +nationality
 
 console.log(myName)
+// Make a variable with your favorite quote or lyric. Change the entire text to uppercase and lowercase in separate steps.
+
 
 let myQuote = "God is love";
 
-myQuote.toUpperCase
+myQuote.toLowerCase
 
-console.log(myQuote.toUpperCase)
-
-
-
+console.log(myQuote.toUpperCase())
+console.log(myQuote.toLowerCase())
 
 
+
+
+
+
+// 6.Create an array of your 5 favorite foods. Display the first and last items in the array.
 
 
 let favoiriteFood = [
@@ -29,8 +34,17 @@ favoiriteFood.push('JILL')
 
 console.log(favoiriteFood)
 
+// 4.Store the price of 3 items in 3 different variables. Calculate the total and display it with a message.
+
+let lambo = 70
+let Kit = 100
+let cloth = 87
+
+console.log(sum)
 
 
+
+// Create three arrays:
 
 const jssOne = ['Henry', 'Daveid', 'linus', 'john', 'Tom', 'Ekpan', 'Geet', 'Maan', 'Dev', 'Sid']
 const jssTwo = ['Fatih', 'Monsu', 'Adi', 'Sasha', 'Chu', 'Emeka', 'Emma', 'Ik', 'Enu', 'Jill']
