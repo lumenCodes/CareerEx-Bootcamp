a4f5807284f5814c19df504c7a33da420cd45c80
Chinwendu <nwazojiechinwendu@gmail.com>
2025-04-19 14:44:24 +0100

    The initial part

---
+++ Assisgnment 4/index.js
@@ -0,0 +1,15 @@
+console.log('Hello World')
+
+// Write a function that takes a student’s score and logs "Passed" if it’s 180 and above, else logs "Failed". (JAMB-style)
+
+
+// Write a function that accepts an age and returns "You are eligible to vote" if the age is 18 or above, otherwise "Not eligible".
+
+
+//Write a function that takes a name and score, then returns:
+// "Excellent" if score >= 90
+// "Good" if score >= 75
+// "Average" if score >= 50
+// "Fail" otherwise
+
+
